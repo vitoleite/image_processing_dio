730e11e68bbd53727a8952af453d9096dd1e1ef8
Vitor Leite <vitudhr@gmail.com>
2022-03-01 20:26:23 -0300

    Adicionando arquivos para aula de packages

---
+++ README.md
@@ -0,0 +1,32 @@
+# Image Processing
+
+## Description.
+
+The package package_name is used to:
+
+- Processing:
+	- Histogram matching
+	- Structural similarity
+	- Resize image
+- Utils:
+	- Read image
+	- Save image
+	- Plot image
+	- Plot result
+	- Plot histogram
+
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install image_processing
+```
+
+
+## Author
+Vitor Leite
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing",
+    version="0.0.1",
+    author="vitor_leite",
+    author_email="vitorleite5@outlook.com",
+    description="Duplicated project to learn how work packages in Python",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
